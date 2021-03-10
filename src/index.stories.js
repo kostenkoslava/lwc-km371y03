39ad75193b45e84e597a80873e01713ca1a0776b
Slava Kostenko <shegan782@gmail.com>
2021-03-10 09:50:08 +0000

    1

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,3 +1,4 @@
+  
 import "@lwc/synthetic-shadow";
 import "https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css";
 import { createElement } from "lwc";
